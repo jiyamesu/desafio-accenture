93199e8e2620f04c73a7d7d1295a38356d488e7c
Joannes Marcos Bercan <joannes.bercan@pdcase.com.br>
2021-05-27 19:54:09 -0300

    Inseridos novos métodos no Controller: Tarefas salva(Tarefas tr), void deleta(Tarefas tr), Tarefas atualiza(Tarefas tr).

--- taskmanager-api/src/main/java/com/acme/taskmanager/controller/TarefasController.java
+++ taskmanager-api/src/main/java/com/acme/taskmanager/controller/TarefasController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,18 +24,28 @@ public class TarefasController {
 	TarefasRepository repository;
 
 	@GetMapping("/tarefas")
-	public List<Tarefas> listaTarefas() {
+	public List<Tarefas> listaTodos() {
 		return repository.findAll();
 	}
-	
+
 	@GetMapping("/tarefa/{id}")
-	public Optional<Tarefas> listaTarefa(@PathVariable (value = "id") Long id) {
+	public Optional<Tarefas> lista(@PathVariable(value = "id") Long id) {
 		return repository.findById(id);
 	}
-	
+
 	@PostMapping("/tarefa")
-	public Tarefas salvaTarefa(@RequestBody Tarefas tr) {
-		return repository.save(tr); 
+	public Tarefas salva(@RequestBody Tarefas tr) {
+		return repository.save(tr);
+	}
+	
+	@DeleteMapping("/tarefa")
+	public void deleta(@RequestBody Tarefas tr) {
+		repository.delete(tr);
+	}
+	
+	@PutMapping("/tarefa")
+	public Tarefas atualiza(@RequestBody Tarefas tr) {
+		return repository.save(tr);
 	}
 
 }
